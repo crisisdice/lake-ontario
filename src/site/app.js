3ab5e57b3f38ed29db049f8b371face1b4a7ea72
Alexander.Daily <alexander.daily@ics-gmbh.de>
2021-05-07 16:41:02 +0200

    basic full stack implementation with websockets

---
+++ src/site/app.js
@@ -0,0 +1,58 @@
+function load_svg() {
+	fetch(new URL("/lake.svg", location.origin))
+		.then(response => response.text())
+		.then(text => {
+			document.getElementById("container").innerHTML = text;
+		});
+}
+
+function handle_click(event) {
+	const id = event.path[0]?.id;
+
+	if (id === null || isNaN(parseInt(id)))
+		return;
+
+	if (ws?.readyState == WebSocket.OPEN)
+		ws.close()
+
+	const request = {
+		node: id,
+		season: document.getElementById("season").value,
+	};
+
+	ws = connect(request);
+}
+
+function connect(request) {
+	const ws = new WebSocket("ws://127.0.0.1:5678/");
+
+	ws.onopen = function() {
+		ws.send(JSON.stringify(request));
+	};
+
+	ws.onmessage = function(message) {
+		let body = JSON.parse(message.data);
+		reset_lake();
+		color_lake(body.nodes);
+	};
+
+	ws.onclose = function() {
+		console.warn("Connection closed");
+	}
+
+	return ws;
+}
+
+function reset_lake() {
+	const lake = Array.prototype.slice.call(document.getElementById("lake").children);
+	lake.forEach(water => {
+		water.style.fill = "rgb(247, 252, 253)";
+	});
+}
+
+function color_lake(nodes) {
+	nodes.forEach(node => {
+		const water = document.getElementById(node.id);
+		water.style.fill = node.color;
+	});
+}
