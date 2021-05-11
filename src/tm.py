5507fddc598203167262a08275d4377e3ef3e1ca
Alexander.Daily <alexander.daily@ics-gmbh.de>
2021-05-11 10:36:00 +0200

    refactor draw function to class, imports in initializer

--- src/tm.py
+++ src/tm.py
@@ -1,4 +1,4 @@
-HEADLESS = False
+HEADLESS = True
 
 import scipy.io
 import matplotlib
@@ -11,77 +11,76 @@ import numpy as np
 import scipy.sparse as sparse
 
 import asyncio
+import functools
 import websockets
 import io
 
-async def draw(websocket=None):
-	mat = scipy.io.loadmat("data/Mtrans.mat")
-	transition_matrix = mat['M']
-	transition_matrix = np.array(transition_matrix)
-	[nn,nn1] = np.shape(transition_matrix)
-	X = np.zeros(nn)
-	X[3065] = 1.0;
-	
-	T = 20 # number time steps 
+class MatrixDraw:
+	def __init__(self):
+		self.websocket = None
+		self.depth = np.loadtxt("data/odepth.dat")
+		self.olon = np.loadtxt("data/olon.dat")
+		self.olat = np.loadtxt("data/olat.dat")
+		self.islake_row = (np.loadtxt("data/islake_row.dat") - 1).astype(int)
+		self.islake_col = (np.loadtxt("data/islake_col.dat") - 1).astype(int)
+		self.glc = np.loadtxt("data/GLcoast.dat")
+		self.transition_matrix = np.array(scipy.io.loadmat("data/Mtrans.mat")['M'])
+		self.dim = np.shape(self.transition_matrix)[0]
 
-	depth = np.loadtxt("data/odepth.dat")
-	olon = np.loadtxt("data/olon.dat")
-	olat = np.loadtxt("data/olat.dat")
-	islake_row = np.loadtxt("data/islake_row.dat")
-	islake_col = np.loadtxt("data/islake_col.dat")
-	glc = np.loadtxt("data/GLcoast.dat")
+	async def draw(self, steps):
+		X = np.zeros(self.dim)
+		X[3065] = 1.0;
 
-	islake_row = islake_row-1
-	islake_col = islake_col-1
-	fig = plt.figure(figsize = (10, 8))
-	lon = glc[:,0]
-	lat = glc[:,1]
+		fig = plt.figure(figsize = (10, 8))
+		lon = self.glc[:,0]
+		lat = self.glc[:,1]
 
-	plt.plot(lon, lat,color = 'black')
-	plt.xlim([np.amin(olon)-0.25, np.amax(olon)+0.25])
-	plt.ylim([np.amin(olat)-0.2, np.amax(olat)+0.2])
-	plt.title('Lake Ontario')
+		plt.plot(lon, lat,color = 'black')
+		plt.xlim([np.amin(self.olon)-0.25, np.amax(self.olon)+0.25])
+		plt.ylim([np.amin(self.olat)-0.2, np.amax(self.olat)+0.2])
+		plt.title('Lake Ontario')
 
-	PD = np.ones(np.shape(depth))*0.0
-	PD[depth == 0] = np.nan
-	mask = np.ones(np.shape(depth))
-	mask[depth == 0] = 0.0
-	PD[depth == 0] = np.nan
+		PD = np.ones(np.shape(self.depth))*0.0
+		PD[self.depth == 0] = np.nan
+		mask = np.ones(np.shape(self.depth))
+		mask[self.depth == 0] = 0.0
+		PD[self.depth == 0] = np.nan
 
-	transition_matrix1 = np.copy(transition_matrix)
+		transition_matrix1 = np.copy(self.transition_matrix)
+		masking = (self.depth>0)
 
-	islake_row = islake_row.astype(int)
-	islake_col = islake_col.astype(int)
+		for i in range (1, steps):
+			t = sparse.csr_matrix(X) * sparse.csr_matrix(transition_matrix1)
+			transition_matrix1 = sparse.csr_matrix(self.transition_matrix) * sparse.csr_matrix(transition_matrix1)
+			t = t.todense()
 
-	masking = (depth>0)
+			PD[self.islake_row, self.islake_col] = t
+			plt.pcolor(self.olon, self.olat, PD, cmap = 'BuGn')
 
-	for i in range (1,T):
-		t = sparse.csr_matrix(X) * sparse.csr_matrix(transition_matrix1)
-		transition_matrix1 = sparse.csr_matrix(transition_matrix) * sparse.csr_matrix(transition_matrix1)
+			if self.websocket is not None:
+				data = io.StringIO()
+				plt.savefig(data, format="svg")
+				print(f"Sending {i} of {steps}")
+				await self.websocket.send(data.getvalue())
 
-		t = t.todense()
-		PD[islake_row,islake_col] = t
-		plt.pcolor(olon,olat,PD,cmap = 'BuGn')
+			else:
+				plt.pause(0.05)
 
-		if websocket is not None:
-			data = io.StringIO()
-			plt.savefig(data, format="svg")
-			print(f"Sending {i} of {T}")
-			await websocket.send(data.getvalue())
-
-		if websocket is None:
-			plt.pause(0.05)
-
-async def process(websocket, path):
-	args = await websocket.recv()
-	print(args)
-	await draw(websocket)
+async def process(websocket, path, md):
+	md.websocket = websocket
+	coords = await websocket.recv()
+	print(coords)
+	await md.draw(20)
 
 if __name__ == "__main__":
+	md = MatrixDraw()
+	print("initialized")
+
 	if HEADLESS:
-		start_server = websockets.serve(process, "127.0.0.1", 5678)
+		handler = functools.partial(process, md = md)
+		start_server = websockets.serve(handler, "127.0.0.1", 5678)
 		asyncio.get_event_loop().run_until_complete(start_server)
 		asyncio.get_event_loop().run_forever()
 	else:
-		asyncio.run(draw())
+		asyncio.run(md.draw(20))
 
