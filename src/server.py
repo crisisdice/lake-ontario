a43b491ae2cc531e4c9b0fc3edef83ae4c0d06cf
Alexander.Daily <alexander.daily@ics-gmbh.de>
2021-05-15 22:47:29 +0200

    logging

--- src/server.py
+++ src/server.py
@@ -1,6 +1,9 @@
 import asyncio
 import functools as f
 import json
+import logging
+import sys
+import traceback
 import websockets
 import websockets.exceptions as wse
 
@@ -8,22 +11,41 @@ from matrixdraw import MatrixDraw
 
 async def handler(websocket, path, artist):
 	try:
+		ip = websocket.remote_address[0]
 		request = await websocket.recv()
 		body = json.loads(request)
 		sv = artist.get_state_vector(body["node"])
-		print(f"Processing {request}")
+
+		logging.info(f"Processing {request} from {ip}")
+
 		for step in range (1, artist.steps):
 			task = asyncio.create_task(artist.draw_frame(sv, step))
 			await websocket.send(json.dumps({ "nodes": await task }))
-
 	except wse.ConnectionClosedError:
-		print("Client closed connection")
+		logging.debug(f"Client {ip} closed connection")
 
-if __name__ == "__main__":
-	md = MatrixDraw()
-	print("Initialized")
-
-	start_server = websockets.serve(f.partial(handler, artist = md), "127.0.0.1", 5678)
+def start(artist):
+	start_server = websockets.serve(f.partial(handler, artist = artist), "127.0.0.1", 5678)
 	asyncio.get_event_loop().run_until_complete(start_server)
 	asyncio.get_event_loop().run_forever()
+
+if __name__ == "__main__":
+	try:
+		logging.basicConfig(
+			format='%(asctime)s [%(threadName)s] %(levelname)s: %(message)s', 
+			level=logging.INFO, 
+			handlers = [
+				logging.FileHandler("logs/server.log"),
+				logging.StreamHandler(sys.stdout)
+			]
+		)
+		md = MatrixDraw()
+
+		logging.info("Initialized and starting server")
+	
+	except Exception:
+		logging.error(f"Not initialized: {traceback.format_exc()}")
+		raise
+
+	start(md)
 
