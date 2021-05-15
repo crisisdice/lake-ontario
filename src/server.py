92963fe4273cd7769d435801e9672764194fd1dd
Alexander.Daily <alexander.daily@ics-gmbh.de>
2021-05-15 20:34:14 +0200

    precalculate powers of transition matrix

--- src/server.py
+++ src/server.py
@@ -5,56 +5,60 @@ import scipy.io
 import scipy.sparse as sparse
 
 import asyncio
-import functools
+import functools as f
 import json
 import websockets
 import websockets.exceptions as wse
 
 class MatrixDraw:
 	def __init__(self):
-		self.transition_matrix = np.array(scipy.io.loadmat("data/Mtrans.mat")['M'])
-		self.dim = np.shape(self.transition_matrix)[0]
-		self.cmap = mpcm.get_cmap("BuGn")
 		self.sensitivity = 0.01
+		self.steps = 30
+		self.cmap = mpcm.get_cmap("BuGn")
+		self.norm = mpcol.Normalize(vmin=self.sensitivity, vmax=self.sensitivity * 10)
+
+		transition_matrix = np.array(scipy.io.loadmat("data/Mtrans.mat")['M'])	
+		self.transition_matricies = self.init_transition_matricies(transition_matrix)
+
+	def init_transition_matricies(self, transition_matrix):
+		initial_transition_matrix = np.copy(transition_matrix)
+		matricies = {}
+
+		for power in range(1, self.steps):
+			iteration = sparse.csr_matrix(transition_matrix) * sparse.csr_matrix(initial_transition_matrix)
+			matricies[str(power)] = iteration
+			initial_transition_matrix = iteration
+		return matricies
 
 	def get_color(self, weight):
-		norm = mpcol.Normalize(vmin=self.sensitivity, vmax=self.sensitivity * 10)
-		rgba = self.cmap(norm(weight), bytes=True)
-
+		rgba = self.cmap(self.norm(weight), bytes=True)
 		return  f"rgb({rgba[0]}, {rgba[1]}, {rgba[2]})"
 
 	def get_state_vector(self, node_id):
-		sv = np.zeros(self.dim)
+		sv = np.zeros(4732)
 		sv[int(node_id)] = 1.0
 		return sv
 
-	async def draw(self, state_vector, steps, websocket):
-		transition_matrix_copy = np.copy(self.transition_matrix)
+	async def draw_frame(self, state_vector, iteration):
+		tm = self.transition_matricies[str(iteration)]
+		values = (sparse.csr_matrix(state_vector) * sparse.csr_matrix(tm)).todense().A1
 
-		for i in range (1, steps):
-			task = asyncio.create_task(self.draw_frame(transition_matrix_copy, state_vector))
-			complete = await task
+		await asyncio.sleep(0.5)
+		return [ { "id": str(i), "color": self.get_color(values[i]) } for i in range(len(values)) if values[i] > self.sensitivity ]
 
-			transition_matrix_copy = complete[0]
-			response = { "nodes": complete[1] }
+	async def handle(self, body, websocket):
+		sv = md.get_state_vector(body["node"])
 
-			await websocket.send(json.dumps(response))
+		for step in range (1, self.steps):
+			task = asyncio.create_task(self.draw_frame(sv, step))
+			await websocket.send(json.dumps({ "nodes": await task }))
 
-	async def draw_frame(self, transition_matrix, state_vector):
-		vector = sparse.csr_matrix(state_vector) * sparse.csr_matrix(transition_matrix)
-		transition_matrix_copy = sparse.csr_matrix(self.transition_matrix) * sparse.csr_matrix(transition_matrix)
-		values = vector.todense().A1
-
-		nodes = [ { "id": str(i), "color": self.get_color(values[i]) } for i in range(len(values)) if values[i] > self.sensitivity ]
-		return (transition_matrix_copy, nodes)
-
-async def process(websocket, path, md):
+async def handler(websocket, path, md):
 	try:
 		message = await websocket.recv()
-		print(f"Processing {message}")
 		body = json.loads(message)
-		sv = md.get_state_vector(body["node"])
-		await md.draw(sv, 30, websocket)
+		print(f"Processing {message}")
+		await md.handle(body, websocket)
 	except wse.ConnectionClosedError:
 		print("Client closed connection")
 
@@ -62,9 +66,7 @@ if __name__ == "__main__":
 	md = MatrixDraw()
 	print("Initialized")
 
-	handler = functools.partial(process, md = md)
-	start_server = websockets.serve(handler, "127.0.0.1", 5678)
-
+	start_server = websockets.serve(f.partial(handler, md = md), "127.0.0.1", 5678)
 	asyncio.get_event_loop().run_until_complete(start_server)
 	asyncio.get_event_loop().run_forever()
 
